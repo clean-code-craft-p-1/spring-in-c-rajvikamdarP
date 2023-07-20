a845238eb74f500c19a443021c413cf81b6cd691
rajvikamdarP <90754422+rajvikamdarP@users.noreply.github.com>
2023-07-20 12:07:43 +0530

    Update stats.cpp
    Added source code.

--- stats.cpp
+++ stats.cpp
@@ -5,4 +5,67 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    int i = 0;
+    int sum = 0;
+
+    if(0 == setlength%2)
+    {
+            if(numberset[0] > numberset[1])
+            {
+                s.max = numberset[0];
+                s.min = numberset[1];
+            }
+            else
+            {
+                s.max = numberset[1];
+                s.min = numberset[0];
+            }
+    }
+    else
+    {
+        s.max = numberset[0];
+        s.min = numberset[0];
+        i=1;
+    }
+
+    while( i < (setlength-1))
+    {
+        if(numberset[i] > numberset[i+1])
+        {
+                if(numberset[i] > s.max)
+                {
+                   s.max = numberset[i];
+                }
+                if(numberset[i+1] < s.min)
+                {
+                   s.min = numberset[i+1];
+                }
+        }
+        else
+        {
+                if(numberset[i+1] > s.max)
+                {
+                   s.max = numberset[i+1];
+                }
+                if(numberset[i] < s.min)
+                {
+                  s.min = numberset[i];
+                }
+        }
+        i+=2;
+    }
+
+    for(i=0; i<setlength; i++)
+    {
+            sum = numberset[i];
+
+    }
+    s.average = sum/setlength;
+    return s;
+}
+
+int main()
+{
+
+}
 }
