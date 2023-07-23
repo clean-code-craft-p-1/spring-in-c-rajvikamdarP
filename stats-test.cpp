a93cd43a05e26aa52597b34e85db4e438e1b0c7e
rajvikamdarP <90754422+rajvikamdarP@users.noreply.github.com>
2023-07-23 21:40:20 +0530

    Updating stats-test for ledalert and email alert messages

--- stats-test.cpp
+++ stats-test.cpp
@@ -14,7 +14,7 @@ TEST(Statistics, ReportsAverageMinMax) {
     EXPECT_LT(abs(computedStats.min - 1.5), epsilon);
 }
 
-#if 0 //temp commenting out the tc
+
 TEST(Statistics, AverageNaNForEmpty) {
     Stats computedStats = compute_statistics(0, 0);
     //All fields of computedStats (average, max, min) must be
@@ -24,24 +24,16 @@ TEST(Statistics, AverageNaNForEmpty) {
     //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
 }
 
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
-
 TEST(Alert, AlertsWhenMaxExceeds) {
-    // create additional .c and .h files
-    // containing functions called emailAlerter, ledAlerter
     alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
-
+    
     float numberset[] = {99.8, 34.2, 4.5};
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
     Stats computedStats = compute_statistics(numberset, setlength);
 
     const float maxThreshold = 10.2;
     check_and_alert(maxThreshold, alerters, computedStats);
-
-    // need a way to check if both emailAlerter, ledAlerter were called
-    // you can define call-counters along with the functions, as shown below
-    EXPECT_EQ(emailAlertCallCount, 1);
-    EXPECT_EQ(ledAlertCallCount, 1);
+    
+    EXPECT_TRUE(emailAlertSent);
+    EXPECT_TRUE(LedAlertOn);
 }
-#endif 
