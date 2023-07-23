3f4e7af0f165a00118c4a26016b54817bec7d59c
rajvikamdarP <90754422+rajvikamdarP@users.noreply.github.com>
2023-07-23 21:45:41 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -9,10 +9,12 @@ typedef struct
 }Stats;
 
 Stats compute_statistics(const float* numberset, int setlength);
+
 // If the email Alert has been sent
-BOOLEAN emailAlertSent = FALSE;
+bool emailAlertSent = FALSE;
 // If the led has turned on
-BOOLEAN LedAlertOn = FALSE;
+bool LedAlertOn = FALSE;
+
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
 //Function for LedAlerter
